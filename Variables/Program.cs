0eb8d2a51cbbf6952d63ad82284eb9ed25a4f1f0
tmartinez114 <tmartinez114@gmail.com>
2020-02-03 00:18:32 -0500

    display range can store in a byte

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,19 +6,7 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Mosh";
-            bool isWorking = true;
-
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
         }
     }
 }
