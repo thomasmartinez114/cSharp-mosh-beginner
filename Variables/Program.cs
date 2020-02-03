0ef3ea2eff8a63eb3775d3957302d80e2ab02e77
tmartinez114 <tmartinez114@gmail.com>
2020-02-03 00:19:24 -0500

    display the byte min and max in float

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -7,6 +7,7 @@ namespace Variables
         static void Main(string[] args)
         {
             Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
         }
     }
 }
