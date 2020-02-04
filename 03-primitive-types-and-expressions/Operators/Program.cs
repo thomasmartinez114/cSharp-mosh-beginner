dce4990d0608926f52e5026204d3825df993e36c
tmartinez114 <tmartinez114@gmail.com>
2020-02-03 19:42:03 -0500

    && operator

--- 03-primitive-types-and-expressions/Operators/Program.cs
+++ 03-primitive-types-and-expressions/Operators/Program.cs
@@ -8,8 +8,10 @@ namespace Operators
         {
             var a = 1;
             var b = 2;
+            var c = 3;
 
-            Console.WriteLine(a > b);
+            Console.WriteLine(c > b && c == a);
         }
     }
 }
+
