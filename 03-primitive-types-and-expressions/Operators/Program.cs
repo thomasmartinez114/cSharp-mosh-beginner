e6e57cd53cad12dc5d2fccab188539fa6ac4ae70
tmartinez114 <tmartinez114@gmail.com>
2020-02-03 19:31:25 -0500

    moved folders into video chapter dir -- add vars

---
+++ 03-primitive-types-and-expressions/Operators/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
