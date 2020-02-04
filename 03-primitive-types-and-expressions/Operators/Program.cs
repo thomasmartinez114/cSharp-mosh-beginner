8abd153cda59a8004e47a40e313ca03d734dfc27
tmartinez114 <tmartinez114@gmail.com>
2020-02-03 19:39:06 -0500

    using PEMDAS

--- 03-primitive-types-and-expressions/Operators/Program.cs
+++ 03-primitive-types-and-expressions/Operators/Program.cs
@@ -6,10 +6,11 @@ namespace Operators
     {
         static void Main(string[] args)
         {
-            var a = 10;
-            var b = 3;
+            var a = 1;
+            var b = 2;
+            var c = 3;
 
-            Console.WriteLine((float)a / (float)b);
+            Console.WriteLine((a + b) * c);
         }
     }
 }
