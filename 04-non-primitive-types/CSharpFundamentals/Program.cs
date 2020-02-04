cb2f602cb1820451e57dd584671ae23965c7f5ac
tmartinez114 <tmartinez114@gmail.com>
2020-02-03 20:05:09 -0500

    Person class

---
+++ 04-non-primitive-types/CSharpFundamentals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+        }
+    }
+}
