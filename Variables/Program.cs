10d0d03c695fb09d30ef026a8b80635b0c1dd2e1
tmartinez114 <tmartinez114@gmail.com>
2020-02-03 19:14:57 -0500

    converted string to boolean

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -8,8 +8,8 @@ namespace Variables
         {
             try
             {
-                var number = "1234";
-                byte b = Convert.ToByte(number);
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
                 Console.WriteLine(b);
             }
             catch (Exception)
