dfd9951e6063a8c82964d6d2af038945f64b5108
tmartinez114 <tmartinez114@gmail.com>
2020-02-04 22:40:43 -0500

    Enum - part 1

--- 04-non-primitive-types/CSharpFundamentals/Program.cs
+++ 04-non-primitive-types/CSharpFundamentals/Program.cs
@@ -4,33 +4,19 @@ using CSharpFundamentals.Math;
 
 namespace CSharpFundamentals
 {
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var firstName = "Mosh";
-            var lastName = "Hamedi";
-
-            var fullName = firstName + " " + lastName;
-
-            // Format Method to define a template for the string with placeholders {0} {1}
-            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
-
-            // Array of names
-            var names = new string[3] { "John", "Jack", "Mary" };
-            var formattedNames = string.Join(",", names);
-            Console.WriteLine(formattedNames);
-
-            // Using string normally is hard to see
-            // var text = "Hi John\nLook into the following paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
-            // Console.WriteLine(text);
-
-            // Use verbatim string
-            var text = @"Hi John
-            Look into the following paths
-            c:\folder1\folder2
-            c:\folder3\folder4";
-            Console.WriteLine(text);
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
         }
     }
 }
