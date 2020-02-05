2776482fa08190b0ccac3edbb8e9f060df7e32bb
tmartinez114 <tmartinez114@gmail.com>
2020-02-04 20:59:22 -0500

    string of names

--- 04-non-primitive-types/CSharpFundamentals/Program.cs
+++ 04-non-primitive-types/CSharpFundamentals/Program.cs
@@ -15,6 +15,11 @@ namespace CSharpFundamentals
 
             // Format Method to define a template for the string with placeholders {0} {1}
             var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            // Array of names
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
         }
     }
 }
