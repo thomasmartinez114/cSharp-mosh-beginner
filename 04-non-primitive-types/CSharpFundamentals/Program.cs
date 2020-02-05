41b439d8933166c3fa9e1c362d2b7fda3f6a6ba8
tmartinez114 <tmartinez114@gmail.com>
2020-02-04 20:48:55 -0500

    strings - Format Method - placeholders {0} {1}

--- 04-non-primitive-types/CSharpFundamentals/Program.cs
+++ 04-non-primitive-types/CSharpFundamentals/Program.cs
@@ -8,24 +8,13 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            // Number arrays
-            var numbers = new int[3];
-            numbers[0] = 1;
+            var firstName = "Mosh";
+            var lastName = "Hamedi";
 
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
+            var fullName = firstName + " " + lastName;
 
-            // Boolean arrays
-            var flags = new bool[3];
-            flags[0] = true;
-
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
-
-            // String arrays
-            var names = new string[3] { "Jack", "John", "Mary" };
+            // Format Method to define a template for the string with placeholders {0} {1}
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
         }
     }
 }
