0128d38a751d6d261d23cfb7a275066c40643440
tmartinez114 <tmartinez114@gmail.com>
2020-02-04 23:06:18 -0500

    Reference types and data values 2

--- 04-non-primitive-types/CSharpFundamentals/Program.cs
+++ 04-non-primitive-types/CSharpFundamentals/Program.cs
@@ -4,26 +4,32 @@ using CSharpFundamentals.Math;
 
 namespace CSharpFundamentals
 {
-    public enum ShippingMethod
+    public class Person
     {
-        RegularAirMail = 1,
-        RegisteredAirMail = 2,
-        Express = 3
+        public int Age;
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var a = 10;
-            var b = a;
-            b++;
-            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
 
-            var array1 = new int[3] { 1, 2, 3 };
-            var array2 = array1;
-            array2[0] = 0;
-            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[1]));
+            var person = new Person() { Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
         }
     }
 }
