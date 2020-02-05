eaf2ef34a60d6082acbf9a4652cff79f8e668b3d
tmartinez114 <tmartinez114@gmail.com>
2020-02-04 20:34:47 -0500

    array

--- 04-non-primitive-types/CSharpFundamentals/Program.cs
+++ 04-non-primitive-types/CSharpFundamentals/Program.cs
@@ -8,14 +8,12 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
         }
     }
 }
