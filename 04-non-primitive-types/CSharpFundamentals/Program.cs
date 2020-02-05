f4ad8b5fd78935df912b24a7b44d1ee6f48931d1
tmartinez114 <tmartinez114@gmail.com>
2020-02-04 20:36:53 -0500

    boolean arrays

--- 04-non-primitive-types/CSharpFundamentals/Program.cs
+++ 04-non-primitive-types/CSharpFundamentals/Program.cs
@@ -14,6 +14,13 @@ namespace CSharpFundamentals
             Console.WriteLine(numbers[0]);
             Console.WriteLine(numbers[1]);
             Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
         }
     }
 }
