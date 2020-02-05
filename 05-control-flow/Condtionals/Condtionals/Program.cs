0d422f3910e25000c5adeb9e6627057e4f9c0de1
tmartinez114 <tmartinez114@gmail.com>
2020-02-04 23:41:26 -0500

    Condtional operator

--- 05-control-flow/Condtionals/Condtionals/Program.cs
+++ 05-control-flow/Condtionals/Condtionals/Program.cs
@@ -8,11 +8,13 @@ namespace Condtionals
         {
             bool isGoldCustomer = true;
 
-            float price;
-            if (isGoldCustomer)
-                price = 19.95f;
-            else
-                price = 29.95f;
+            //float price;
+            //if (isGoldCustomer)
+            //    price = 19.95f;
+            //else
+            //    price = 29.95f;
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
         }
     }
 }
