20aa3e906b1294be01f5274c154d17174b3a085e
tmartinez114 <tmartinez114@gmail.com>
2020-02-06 00:06:00 -0500

    Assignment 1 - If/else valid or invalid number input

--- 05-control-flow/assignment/assignment/Program.cs
+++ 05-control-flow/assignment/assignment/Program.cs
@@ -4,9 +4,15 @@ namespace assignment
 {
     class Program
     {
-        static void Main(string[] args)
+        public void Exercise1()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number: ");
+            var number = Console.ReadLine();
+            var convert = Convert.ToInt32(number);
+            if (number >= 1 && number <= 10)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("Invalid");
         }
     }
 }
