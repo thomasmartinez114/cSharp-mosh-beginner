584d15ee23ae0688bae269931746c0953865c266
tmartinez114 <tmartinez114@gmail.com>
2020-02-05 23:36:13 -0500

    Ifelse - switch cases

--- 05-control-flow/Condtionals/Condtionals/Program.cs
+++ 05-control-flow/Condtionals/Condtionals/Program.cs
@@ -2,19 +2,23 @@
 
 namespace Condtionals
 {
-        public enum Season
-        {
-            Spring,
-            Summer,
-            Autumn,
-            Winter
-        }
-
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.Summer:
+                    Console.WriteLine("We've got promotion.");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+            }
         }
     }
 }
---
+++ 05-control-flow/Condtionals/Condtionals/Season.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Condtionals
+{
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+}
