380e321b466b6b270306808de633d7d0913cba8e
tmartinez114 <tmartinez114@gmail.com>
2020-02-06 00:18:59 -0500

    foreach to iterate through array

--- 05-control-flow/Iterations/Program.cs
+++ 05-control-flow/Iterations/Program.cs
@@ -6,16 +6,11 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            var name = "John Smith";
+            var numbers = new int[] { 1, 2, 3, 4 };
 
-            //for (var i = 0; i < name.Length; i++)
-            //{
-            //    Console.WriteLine(name[i]);
-            //}
-
-            foreach (var character in name)
+            foreach (var number in numbers)
             {
-                Console.WriteLine(character);
+                Console.WriteLine(number);
             }
         }
     }
