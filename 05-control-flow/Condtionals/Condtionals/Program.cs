0d35c869f2f7bbb8ff988a274b26b53f61417c5b
tmartinez114 <tmartinez114@gmail.com>
2020-02-05 23:16:10 -0500

    enum

--- 05-control-flow/Condtionals/Condtionals/Program.cs
+++ 05-control-flow/Condtionals/Condtionals/Program.cs
@@ -2,19 +2,19 @@
 
 namespace Condtionals
 {
+        public enum Season
+        {
+            Spring,
+            Summer,
+            Autumn,
+            Winter
+        }
+
     class Program
     {
         static void Main(string[] args)
         {
-            bool isGoldCustomer = true;
-
-            //float price;
-            //if (isGoldCustomer)
-            //    price = 19.95f;
-            //else
-            //    price = 29.95f;
-
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            
         }
     }
 }
