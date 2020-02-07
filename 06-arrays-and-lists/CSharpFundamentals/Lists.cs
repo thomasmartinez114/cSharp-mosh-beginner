b9cf13760d62523121cc1b79845212ae257f1568
tmartinez114 <tmartinez114@gmail.com>
2020-02-06 21:14:03 -0500

    created Lists with Add, AddRange

---
+++ 06-arrays-and-lists/CSharpFundamentals/Lists.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpFundamentals
+{
+    class Lists
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+        }
+    }
+}
