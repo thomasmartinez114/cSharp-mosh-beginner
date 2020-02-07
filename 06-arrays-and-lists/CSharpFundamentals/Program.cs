a390a1f3ffdabdb3ed3afe4b789f9669b4493bda
tmartinez114 <tmartinez114@gmail.com>
2020-02-06 20:10:47 -0500

    Length of arr

---
+++ 06-arrays-and-lists/CSharpFundamentals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 3, 6, 4, 14, 5 };
+
+            // Length
+            Console.WriteLine("Length: " + numbers.Length);
+        }
+    }
+}
