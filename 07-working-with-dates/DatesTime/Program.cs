2e4e0bb4080991823035510324fc7acb9209f357
tmartinez114 <tmartinez114@gmail.com>
2020-02-08 18:47:52 -0500

    long/short date string, long/short time string

--- 07-working-with-dates/DatesTime/Program.cs
+++ 07-working-with-dates/DatesTime/Program.cs
@@ -10,12 +10,21 @@ namespace DatesTime
             var now = DateTime.Now;
             var today = DateTime.Today;
 
-            Console.WriteLine(now);
-            Console.WriteLine();
-            Console.WriteLine(today);
-            Console.WriteLine();
-            Console.WriteLine("Hour: " + now.Hour);
-            Console.WriteLine("Minute: " + now.Minute);
+            //Console.WriteLine(now);
+            //Console.WriteLine();
+            //Console.WriteLine(today);
+            //Console.WriteLine();
+            //Console.WriteLine("Hour: " + now.Hour);
+            //Console.WriteLine("Minute: " + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString()); // Saturday, February 8, 2020
+            Console.WriteLine(now.ToShortDateString()); // 2/8/2020
+            Console.WriteLine(now.ToLongTimeString()); // 6:46:33 PM
+            Console.WriteLine(now.ToShortTimeString()); // 6:46 PM
+            
         }
     }
 }
