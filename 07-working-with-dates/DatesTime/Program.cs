54058054e3c183b44173582b2051000c10cd7f84
tmartinez114 <tmartinez114@gmail.com>
2020-02-08 18:37:14 -0500

    show dateTime, now, today

---
+++ 07-working-with-dates/DatesTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DatesTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine(now);
+            Console.WriteLine();
+            Console.WriteLine(today);
+            Console.WriteLine();
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+        }
+    }
+}
