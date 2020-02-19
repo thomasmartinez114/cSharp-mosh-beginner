05177fc224777a60f3487199b3a73367beabfa3e
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-18 20:48:29 -0500

    Trim() white space in name

---
+++ 08-working-with-text/CSharpFundamentals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Thomas Martinez ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+        }
+    }
+}
