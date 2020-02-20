7fc5e24f25986e1d5f67f064ed849bb5703c9f12
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-19 23:15:38 -0500

    first name last name by using split()

--- 08-working-with-text/CSharpFundamentals/Program.cs
+++ 08-working-with-text/CSharpFundamentals/Program.cs
@@ -19,6 +19,10 @@ namespace CSharpFundamentals
             var lastName = fullName.Substring(index + 1);
             Console.WriteLine("FirstName: " + firstName);
             Console.WriteLine("LastName: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
         }
     }
 }
