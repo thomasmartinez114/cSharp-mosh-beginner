16b2fc14441c507a6be892231e5547d2e13f24fb
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-20 19:15:25 -0500

    Truncating sentence

--- 08-working-with-text/CSharpFundamentals/Program.cs
+++ 08-working-with-text/CSharpFundamentals/Program.cs
@@ -10,6 +10,7 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
+            /*
             var fullName = "Thomas Martinez ";
             Console.WriteLine("Trim: '{0}'", fullName.Trim());
             Console.WriteLine("Trim: '{0}'", fullName.Trim().ToUpper());
@@ -37,6 +38,31 @@ namespace CSharpFundamentals
             // Convert # to str
             float price = 29.95f;
             Console.WriteLine(price.ToString("C0"));
+            */
+
+            var sentence = "This is a very long long long long long long long long long long text.";
+            const int maxLength = 20;
+
+            if (sentence.Length < maxLength)
+                Console.WriteLine(sentence);
+            else
+            {
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxLength)
+                        break;
+                }
+
+                var summary = String.Join(' ', summaryWords) + '...';
+                Console.WriteLine(summary);
+            }
         }
     }
 }
