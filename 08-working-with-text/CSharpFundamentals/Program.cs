86dd2736dff880cd6a85c1f17dd75f90d03686f7
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-20 19:30:18 -0500

    Added another class and made it public to use within Program.cs

--- 08-working-with-text/CSharpFundamentals/Program.cs
+++ 08-working-with-text/CSharpFundamentals/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CSharpFundamentals
 {
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
@@ -41,29 +41,10 @@ class Program
         */
 
         var sentence = "This is a very long long long long long long long long long long text.";
-        var summary = SummerizeText(sentence);
+        var summary = StringUtility.SummerizeText(sentence);
             Console.WriteLine(summary);
     }
 
-    static string SummerizeText(string text, int maxLength = 20)
-        {
-
-        if (text.Length < maxLength)
-            return text;
-            var words = text.Split(' ');
-            var totalCharacters = 0;
-            var summaryWords = new List<string>();
-
-            foreach (var word in words)
-            {
-                summaryWords.Add(word);
-
-                totalCharacters += word.Length + 1;
-                if (totalCharacters > maxLength)
-                    break;
-            }
-
-            return String.Join(' ', summaryWords) + '...';
-        }
+    
     }
 }
