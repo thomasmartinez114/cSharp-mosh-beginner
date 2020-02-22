6476fe77ac16c18dc90f6ac1b4925a06da87251b
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-21 19:55:53 -0500

    began string demo

---
+++ 08-working-with-text/String-demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace String_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.Append('-', 10);
+            Console.WriteLine(builder);
+        }
+    }
+}
