11c68dc81ad5a5a1f9651ee1056716138932a028
thomasmartinez114 <tmartinez114@gmail.com>
2020-02-22 22:48:57 -0500

    File.Copy()

---
+++ 09-working-with-files/File-and-fileinfo-demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace File_and_fileinfo_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Copy(@"c:\temp\myfile.jpg", "d:\temp\myfile.jpg", true)
+        }
+    }
+}
